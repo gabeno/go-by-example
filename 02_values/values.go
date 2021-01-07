6244283487d402ce46179535772fcc15ed0ff959
Gabriel Majivu <gabriel@angaza.com>
2021-01-07 10:08:51 +0300

    values
    - strings
    - booleans
    - integers and floats
    
    ```
    $ go run 02_values/values.go
    golang
    1 + 1 =  2
    7.0 / 3.0 =  2.3333333333333335
    false
    true
    false
    ```

---
+++ 02_values/values.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	// strings
+	fmt.Println("go" + "lang")
+
+	// integers and floats
+	fmt.Println("1 + 1 = ", 1+1)
+	fmt.Println("7.0 / 3.0 = ", 7.0/3.0)
+
+	// booleans and boolean operators
+	fmt.Println(true && false)
+	fmt.Println(true || false)
+	fmt.Println(!true)
+}
