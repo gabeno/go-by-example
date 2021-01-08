6c57e69ba2543bbbab13ecb78060d7fc6579cfc2
Gabriel Majivu <gabriel@angaza.com>
2021-01-08 11:22:51 +0300

    switch
    ```
    $ go run 07_switch/switch.go
    Write 2 as two
    It's a weekday
    It's before noon
    I'm a bool
    I'm an int
    Don't know type string
    ```

---
+++ 07_switch/switch.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	// basic switch
+	i := 2
+	fmt.Print("Write ", i, " as ")
+	switch i {
+	case 1:
+		fmt.Println("one")
+	case 2:
+		fmt.Println("two")
+	case 3:
+		fmt.Println("three")
+	}
+
+	// we can use multiple expressions on the same case statement
+	// we can also use optional default case
+	switch time.Now().Weekday() {
+	case time.Saturday, time.Sunday:
+		fmt.Println("It's the weekend")
+	default:
+		fmt.Println("It's a weekday")
+	}
+
+	// switch w/o expression is an alternate way to express if/else logic
+	// case expressions can also be non-constants
+	t := time.Now()
+	switch {
+	case t.Hour() < 12:
+		fmt.Println("It's before noon")
+	default:
+		fmt.Println("It's after noon")
+	}
+
+	// a type switch compares types instead of values
+	// the variable t will have the type corresponding to to its clause
+	whatAmI := func(i interface{}) {
+		switch t := i.(type) {
+		case bool:
+			fmt.Println("I'm a bool")
+		case int:
+			fmt.Println("I'm an int")
+		default:
+			fmt.Printf("Don't know type %T\n", t)
+		}
+	}
+
+	whatAmI(true)
+	whatAmI(1)
+	whatAmI("hey")
+}
